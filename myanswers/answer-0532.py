b23b9068bee31e259f665f30b1f31de0b913c3ac
calosetrujillop <carlose.trujillop@udea.edu.co>
2026-05-14 10:07:36 -0500

    Update answer-0532.py

--- myanswers/answer-0532.py
+++ myanswers/answer-0532.py
@@ -1,1 +1,8 @@
+import numpy as np
+from sklearn.decomposition import PCA
 
+def optimizar_pca_señales(X, varianza_objetivo):
+    pca = PCA().fit(X)
+    cum_var = np.cumsum(pca.explained_variance_ratio_)
+    n_comp = np.argmax(cum_var >= varianza_objetivo) + 1
+    return (int(n_comp), cum_var)
